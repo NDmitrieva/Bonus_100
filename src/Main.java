156484961ac3f56fb655c6a7d39f0ea724cc2a68
NDmitrieva <al_amanecer@mail.ru>
2021-10-02 16:51:19 +0300

    added bonus

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = 100;
+        int add = 1100;
+        int bonus; {
+            bonus = add / 100;
+            if (add>=1000);
+            else bonus = 0;
+        }
+        int totalSum = sum + add + bonus;
+
+        System.out.println("Сумма счета: " + totalSum + " руб.");
+        System.out.println("Бонус составил: " + bonus + " руб.");
+
+
+    }
+}
